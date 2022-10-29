dcd514c99e81ee74efe4ea6a46a150803cee413b
Winnieye0206 <winnieye.99@gmail.com>
2022-10-30 00:15:39 +1100

    Gender and background

--- Referral Letter.js
+++ Referral Letter.js
@@ -21,7 +21,14 @@ function referralLetter(name, gender, injuryArea, handDominance, occupation, inj
 function createReferralLetter() {
     let letter = document.getElementById("letter")
     let name = document.getElementById('Name').value
-    let gender = document.getElementById('Gender').value
+    let gender
+    if (document.getElementById('his').checked == true) {
+        gender = document.getElementById('his').value
+    } else if(document.getElementById('her').checked == true) {
+        gender = document.getElementById('her').value
+    } else {
+        gender = 'their'
+    }
     let injuryArea = document.getElementById('Injury area').value
     let handDominance = document.getElementById('Hand dominance').value
     let occupation = document.getElementById('Occupation').value
